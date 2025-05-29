2a1c85496f5bfa212d5d1421cb04995b30868b5a
Joan Nyawira Kori <korijoan171@gmail.com>
2025-05-29 13:37:31 +0300

    Project code implementation guide

--- lib/codersinfo.py
+++ lib/codersinfo.py
@@ -1,9 +1,32 @@
 #In helpers.py
 #Services implementation runs from line 10-13
+    #getting all services:9-13
 
-#Providers implementaion runs from line
+#Providers implementation runs from line 15 -103
+    #getting all providers:16-21
+    #getting providers by name:23-31
+    #adding a new provider:33-70
+    #deleting providers:72- 103
 
-#Users implementaion runs from line
+#Users implementation runs from line  105-170
+    #getting all users:106-110
+    #getting user by name:112-120
+    #adding a new user:122-138
+    #deleting users:140 -171
 
-#Booking implementaion runs from line
+#Booking implementation runs from line 173-311
+    #getting bookings all: 174-181
+    #getting bookings by location: 183-199 
+    #adding a new booking: 202-282
+    #deleting bookings: line 284-311
 
+
+
+#In models.py
+#Service class runs from line 12 -27
+
+#Provider class runs from line 29-54
+
+#User class runs from line 56-76
+
+#Booking class runs from line 78-97
